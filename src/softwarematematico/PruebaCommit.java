94314cf4bf34321f2322ac362eb847de912ae2a0
Olijoel0411 <oliverloconlopez@gmail.com>
2022-09-12 15:31:09 -0600

    Version Laptop...

--- src/softwarematematico/PruebaCommit.java
+++ src/softwarematematico/PruebaCommit.java
@@ -8,5 +8,6 @@ public class PruebaCommit {
     public static void main(String[] args) {
         System.out.println("Haciendo Commit");
         System.out.println("Version Laptop");
+        System.out.println("Prueba de merge");
     }
 }
