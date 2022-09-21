8da7e15924469e0576e8df39ac6d76f47ab6d171
OliJoel0411 <106788709+OliJoel0411@users.noreply.github.com>
2022-09-20 20:41:46 -0600

    VSC

--- src/softwarematematico/PruebaCommit.java
+++ src/softwarematematico/PruebaCommit.java
@@ -7,5 +7,8 @@ package softwarematematico;
 public class PruebaCommit {
     public static void main(String[] args) {
         System.out.println("Haciendo Commit");
+        /*
+         VSC
+         */
     }
 }
